e51d3e2a05a5a0ebdbaccd260966bd6cf14271a9
retr0 <tanfelix.richard@gmail.com>
2022-08-25 13:22:28 +0800

    :sparkles: added questions and answers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ctr">
-    <Questions />
-    <Result />
+    <Questions v-if="questionsAnswered < questions.length" :questions="questions"/>
+    <Result v-else/>
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
@@ -19,6 +19,7 @@ export default {
 
   data() {
     return {
+      questionsAnswered: 0,
       questions: [
         {
           q: "What is 2 + 2?",
